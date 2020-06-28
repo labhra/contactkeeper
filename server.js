423e369d334d1ae5d6ad1b459086bf2bd75f6b83
Laura <lauraduggan89@gmail.com>
2020-06-28 16:25:21 +0800

    updated prod route"

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("./routes/contacts");
 
 const app = express();
 
@@ -17,6 +18,14 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 
 app.use("/api/contacts", require("./routes/contacts"));
+
+//serv static assets in PROD
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5555;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
